ffd1dc1cd49577f32960e914923b6775770084be
anderson <duonganderson@gmail.com>
2021-10-12 19:48:58 -0400

    Search function to find artist URI

--- scripts/playlist.py
+++ scripts/playlist.py
@@ -1,25 +1,32 @@
-import sys, spotipy
+import sys, spotipy, json
 from spotipy.oauth2 import SpotifyOAuth, SpotifyClientCredentials
 
+# Initiliazes Spotipy client with valid scope, credentials
 def init_client():
 
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 1: # Checks if user inputs valid username
         username = sys.argv[1]
     else:
-        print('You need a username!')
-        sys.exit()
+        print('You need a username!') 
+        sys.exit() # Exits script if username is not adequately provided
 
-    SPOTIPY_REDIRECT_URI='localhost:8000'
+    scope = 'playlist-modify-public' # Write access to user's public playlists
 
-    scope = 'playlist-modify-private'
-
-    client_credentials = SpotifyClientCredentials()
-    auth_manager = SpotifyOAuth(redirect_uri=SPOTIPY_REDIRECT_URI, scope=scope, username=username)
-    sp = spotipy.Spotify(auth=auth_manager, client_credentials_manager=client_credentials)
-    print('Successful authentication!')
+    client_credentials = SpotifyClientCredentials() # API keys stored in environmental variables
+    auth_manager = SpotifyOAuth(username=username, scope=scope) 
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials, auth_manager=auth_manager)
 
     return sp
+
+# Searches for associated URI for artist
+def search_artist(sp, artist_name):
+
+    query_artist_name = artist_name.replace(" ", "+") # Spotify query requires spaces encode with hex code %20 or +\
+    result = sp.search(q=query_artist_name, limit=1, type='artist') 
+
+    return result['artists']['items'][0]['uri'] # Spotify URI for search artist
 
 if __name__ == '__main__':
 
     sp = init_client()
+    URI = search_artist(sp, 'tyler the creator')
